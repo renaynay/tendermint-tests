138b492b7568867dd8717c5f5afd66007ad61f26
rene <41963722+renaynay@users.noreply.github.com>
2021-07-12 16:39:11 +0200

    go sdk

---
+++ large_block_consensus/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/docker/docker/api/types"
+	container_types "github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+	"github.com/docker/docker/container"
+	"os"
+)
+
+var (
+	ctx = context.Background()
+	tendermintImage = "tendermint/tendermint:latest"
+
+	seedIP string
+)
+
+func main() {
+	// get docker client
+	cli, err := client.NewClientWithOpts(nil)
+	if err != nil {
+		os.Exit(1)
+	}
+	// spin up seed node
+	_, err = spinUpSeedNode(cli)
+	// get peerID@IP:PORT of seed node
+	// set it as ENV VAR
+	// feed it to other clients
+}
+
+func spinUpSeedNode(cli *client.Client) (*container.Container, error) {
+	_, err := cli.ImageBuild(ctx, nil, types.ImageBuildOptions{
+		Tags: []string{"seed"},
+		Dockerfile: tendermintImage,
+
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = cli.ContainerCreate(ctx, &container_types.Config{
+		Image: "seed",
+	}, nil, nil, nil, "seed")
+	if err != nil {
+		return nil, err
+	}
+
+	containers, err := cli.ContainerList(ctx, types.ContainerListOptions{All: true})
+	if err != nil {
+		return nil, err
+	}
+	for _, container := range containers {
+		fmt.Println(container.Names)
+	}
+	return nil, nil
+}
+
+
