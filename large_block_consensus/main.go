573119cdc3c06c7854a8b5a610bcfa4357e02d8e
rene <41963722+renaynay@users.noreply.github.com>
2021-07-13 14:20:53 +0200

    try

--- large_block_consensus/main.go
+++ large_block_consensus/main.go
@@ -4,12 +4,8 @@ import (
 	"context"
 	"fmt"
 	"os"
-	"path/filepath"
 
-	"github.com/docker/docker/api/types"
-	container_types "github.com/docker/docker/api/types/container"
-	"github.com/docker/docker/client"
-	"github.com/docker/docker/container"
+	docker "github.com/fsouza/go-dockerclient"
 )
 
 var (
@@ -21,54 +17,46 @@ var (
 
 func main() {
 	// get docker client
-	cli, err := client.NewClientWithOpts()
+	client, err := docker.NewClient("unix:///var/run/docker.sock")
 	if err != nil {
 		fmt.Println("couldnt set up client: ", err)
 		os.Exit(1)
 	}
-	fmt.Println("made docker client", cli.ClientVersion())
+	fmt.Println("made docker client: ", client.Endpoint())
 	// spin up seed node
-	_, err = spinUpSeedNode(cli)
+	seed, err := spinUpSeedNode(client)
 	if err != nil {
 		fmt.Println("couldn't spin up seed node: ", err)
 		os.Exit(1)
 	}
-	fmt.Println("spun up seed node?")
+	fmt.Println("spun up seed node!!! Seed ID: ", seed.ID)
 	// get peerID@IP:PORT of seed node
 	// set it as ENV VAR
 	// feed it to other clients
 }
 
-func spinUpSeedNode(cli *client.Client) (*container.Container, error) {
-	path, err := filepath.Abs("./tendermint-seed/Dockerfile")
-	tendermintSeedFile, err := os.OpenFile(path, os.O_RDONLY, os.ModeTemporary)
-	if err != nil {
-		return nil, err
-	}
-	defer tendermintSeedFile.Close()
-
-	resp, err := cli.ImageBuild(ctx, tendermintSeedFile, types.ImageBuildOptions{
-		Tags:       []string{"seed"},
-		Dockerfile: "Dockerfile",
+func spinUpSeedNode(client *docker.Client) (*docker.Container, error) {
+	seedContainer, err := client.CreateContainer(docker.CreateContainerOptions{
+		Name: "seed",
+		Config: &docker.Config{
+			Image: tendermintImage,
+			User: "root",
+			Mounts: []docker.Mount{
+				{
+					Source: "./tendermint-seed/init/docker-entrypoint.sh",
+					Destination: "/usr/local/bin/docker-entrypoint.sh",
+				},
+				{
+					Source: "./tendermint-seed/init/node_key.json",
+					Destination: "~/config/node_key.json",
+				},
+			},
+		},
 	})
 	if err != nil {
 		return nil, err
 	}
-	defer resp.Body.Close()
 
-	_, err = cli.ContainerCreate(ctx, &container_types.Config{
-		Image: "seed",
-	}, nil, nil, nil, "seed")
-	if err != nil {
-		return nil, err
-	}
-
-	containers, err := cli.ContainerList(ctx, types.ContainerListOptions{All: true})
-	if err != nil {
-		return nil, err
-	}
-	for _, container := range containers {
-		fmt.Println(container.Names)
-	}
-	return nil, nil
+	fmt.Println("created container!!!!!!!: ", seedContainer.Created)
+	return seedContainer, nil
 }
